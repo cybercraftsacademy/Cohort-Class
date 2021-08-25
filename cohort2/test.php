eaeeac3d052f08eac954d6b23c162c95ac16f127
Cyber Crafts Academy <89128741+cybercraftsacademy@users.noreply.github.com>
2021-08-25 12:09:44 +0300

    Add files via upload

---
+++ cohort2/test.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+</head>
+<body>
+
+<?php
+
+$ancent = 5;
+$mario = 26;
+
+$sum = $ancent + $mario;
+echo "The sum is".$sum;
+
+?>
+
+<?php
+
+$firstName = "Peter";
+$secondName = "Bob";
+
+
+//Good morning fullname 
+
+$fullname = $firstName." ".$secondName;
+
+echo "Good morning <b>$firstName $secondName</b>";
+
+echo "<br>";
+echo "Good morning ".$fullname;
+
+?>
+
+</body>
+</html>
